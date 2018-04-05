3caf80b0638f2605c18d9d06a2f3485a393c71e2
Raja Jawahar <rajajawahar77@gmail.com>
2018-04-05 13:11:06 +0530

    Initial Commit

---
+++ iOSMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSMVVM
+//
+//  Created by Raja Jawahar on 05/04/18.
+//  Copyright © 2018 Raja Jawahar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
