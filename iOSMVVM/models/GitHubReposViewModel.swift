87724db136cdaf12862f6c391aef9bc51fe46b24
Raja Jawahar <rajajawahar77@gmail.com>
2018-06-06 11:26:48 +0530

    GitHub Repos and UserViewModel added

---
+++ iOSMVVM/models/GitHubReposViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubReposViewModel.swift
+//  iOSMVVM
+//
+//  Created by Raja Jawahar on 06/06/18.
+//  Copyright © 2018 Raja Jawahar. All rights reserved.
+//
+
+import UIKit
+
+class GitHubReposViewModel: NSObject {
+
+}
---
+++ iOSMVVM/models/GitHubUserViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  EmployeeViewModel.swift
+//  iOSMVVM
+//
+//  Created by Raja Jawahar on 05/04/18.
+//  Copyright © 2018 Raja Jawahar. All rights reserved.
+//
+
+import UIKit
+
+struct GitHubUserViewModel {
+    
+    var login : String?
+    var id : Int32?
+    var avatarUrl : String?
+    var followers_url : String?
+    var repos_url : String?
+
+}
