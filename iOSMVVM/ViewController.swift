3d5e37cbd24915620bc72c19b27a6300d7776f32
Raja Jawahar <rajajawahar77@gmail.com>
2020-09-13 22:00:20 +0530

    UserList ViewModel modified

--- iOSMVVM/ViewController.swift
+++ iOSMVVM/ViewController.swift
@@ -10,49 +10,34 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return userList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "userList") as! UserTableViewCell
-        let user = userList[indexPath.row]
-        cell.name.text = user.login
-        cell.url.text = user.followersUrl
-        cell.avatar.imageFromUrl(urlString: user.avatarUrl!)
-        cell.avatar.layer.cornerRadius = cell.avatar.frame.size.width / 2;
-        cell.avatar.layer.masksToBounds = true;
-        return cell
-        
-    }
-    
-    @IBOutlet weak var userTableView: UITableView!
-    
-    var apiManager : APIManager?
-    var userList  = [User]()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        userTableView.delegate = self
-        userTableView.dataSource = self
-        apiManager = APIManager()        
-        Alamofire.request("https://api.github.com/users")
-            .responseJSON { response in
-                do {
-                    let decoder = JSONDecoder()
-                    let user = try decoder.decode([User].self, from: response.data!)
-                    self.userList.append(contentsOf: user)
-                    self.userTableView.reloadData()
-                } catch let err {
-                    print("Err", err)
-                }
-        }
-        
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+  @IBOutlet weak var userTableView: UITableView!
+  
+  var userListViewModel = UserListViewModel()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    userTableView.delegate = self
+    userTableView.dataSource = self
+    userListViewModel.fetchUserList { [weak self]  in
+      self?.userTableView.reloadData()
     }
-
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return userListViewModel.users.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "userList") as! UserTableViewCell
+    let user = userListViewModel.users[indexPath.row]
+    cell.name.text = user.login
+    cell.url.text = user.followersUrl
+    cell.avatar.imageFromUrl(urlString: user.avatarUrl!)
+    cell.avatar.layer.cornerRadius = cell.avatar.frame.size.width / 2;
+    cell.avatar.layer.masksToBounds = true;
+    return cell
+  }
+  
 }
 
--- iOSMVVM/viewmodels/UserListViewModel.swift
+++ iOSMVVM/viewmodels/UserListViewModel.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
-struct UserListViewModel {
-    
-    var users :[User]
-    init(users :[User]) {
-        self.users = users
+class UserListViewModel {
+  
+  var users = [User]()
+  
+  func fetchUserList(completion: @escaping() -> Void) {
+    Alamofire.request("https://api.github.com/users")
+      .responseJSON { response in
+        do {
+          let decoder = JSONDecoder()
+          let user = try decoder.decode([User].self, from: response.data!)
+          self.users.append(contentsOf: user)
+          completion()
+        } catch let err {
+          print("Err", err)
+        }
     }
+  }
 }
+
+
